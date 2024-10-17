3ce0e2968c37268c2f251debd6c9a75ff7db90a5
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-10-17 18:55:46 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const car = {
+    type: "Sedan",
+    model: "Camry",
+    color: "Blue"
+};
+
+console.log("Type of car:", typeof car);
+
+car.type = "Toyota";
+console.log("Updated car object:", car);
+
+car.wheels = 4;
+console.log("Car object after adding wheels property:", car);
